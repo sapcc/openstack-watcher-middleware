c1904f5c9a3c422f5f3a22ae5b4b755f40f4dce8
Arno Uhlig <arno.uhlig@sap.com>
2018-09-06 17:11:44 +0200

    also add cadf service name, initiator project domain id, initiator user domain id to environ

--- watcher/watcher.py
+++ watcher/watcher.py
@@ -136,8 +136,8 @@ class OpenStackWatcherMiddleware(object):
         req = Request(environ)
 
         # determine initiator based on token
-        initiator_project_id, initiator_domain_id, initiator_user_id = \
-            self.get_initiator_project_domain_user_uid_from_environ(environ)
+        initiator_project_id, initiator_project_domain_id, initiator_domain_id, initiator_user_id, \
+            initiator_user_domain_id = self.get_initiator_project_domain_user_uid_from_environ(environ)
         initiator_host_address = req.client_addr or taxonomy.UNKNOWN
 
         # determine target based on request path or keystone.token_info
@@ -164,15 +164,20 @@ class OpenStackWatcherMiddleware(object):
 
         # set environ for initiator
         environ['WATCHER.INITIATOR_PROJECT_ID'] = initiator_project_id
+        environ['WATCHER.INITIATOR_PROJECT_DOMAIN_ID'] = initiator_project_domain_id
         environ['WATCHER.INITIATOR_DOMAIN_ID'] = initiator_domain_id
         environ['WATCHER.INITIATOR_USER_ID'] = initiator_user_id
+        environ['WATCHER.INITIATOR_USER_DOMAIN_ID'] = initiator_user_domain_id
         environ['WATCHER.INITIATOR_HOST_ADDRESS'] = initiator_host_address
 
         # set environ for target
         environ['WATCHER.TARGET_PROJECT_ID'] = target_project_id
         environ['WATCHER.TARGET_TYPE_URI'] = target_type_uri
+
+        # general cadf attributes
         environ['WATCHER.ACTION'] = cadf_action
         environ['WATCHER.SERVICE_TYPE'] = self.service_type
+        environ['WATCHER.CADF_SERVICE_NAME'] = self.strategy.get_cadf_service_name()
 
         # labels applied to all metrics emitted by this middleware
         labels = [
@@ -245,19 +250,21 @@ class OpenStackWatcherMiddleware(object):
 
     def get_initiator_project_domain_user_uid_from_environ(self, environ):
         """
-        get the project uid, domain uid, user uid from the environ
+        get the project uid, project domain uid, domain uid, user uid, user domain uid from the environ
         as parsed by the keystone.auth_token middleware
 
         :param environ: the request's environ
-        :return: project, domain, user uid
+        :return: project id, project domain id, domain id, user uid, user domain id
         """
-        project_id = domain_id = user_id = taxonomy.UNKNOWN
+        project_id = project_domain_id = domain_id = user_id = user_domain_id = taxonomy.UNKNOWN
         try:
             project_id = environ.get('HTTP_X_PROJECT_ID')
+            project_domain_id = environ.get('HTTP_X_PROJECT_DOMAIN_ID')
             domain_id = environ.get('HTTP_X_DOMAIN_ID')
             user_id = environ.get('HTTP_X_USER_ID')
+            user_domain_id = environ.get('HTTP_X_USER_DOMAIN_ID')
         finally:
-            return project_id, domain_id, user_id
+            return project_id, project_domain_id, domain_id, user_id, user_domain_id
 
     def get_target_project_uid_from_path(self, path):
         """
